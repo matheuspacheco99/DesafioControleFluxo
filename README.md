62c19212f95de45803ebb219854537e59db536c0
matheuspacheco99 <matheussouzap99@gmail.com>
2024-07-10 21:33:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DIO - Trilha Java Básico
+Site: <a href="https://www.dio.me/" target="_blank">DIO</a>
+
+## Desafio - Controle de Fluxo
+Este é um Desafio simples no qual é usado dois parâmetros inteiros e calcula a contagem entre eles,na qual é usado o conhecimento aprendido referente ao controle de fluxo como leitura de entrada,tratamento de exceções.
+
+### Exceção utilizada
+Uma exceção foi criada para que caso o segundo parâmetro fornecido seja menor que o primeiro parâmetro, uma exceção chamada "ParametrosInvalidosException" será chamada pois o segundo parâmetro deve ser maior que o primeiro.
+
+#### Autor - [Matheus Pacheco](https://github.com/matheuspacheco99)  
---
+++ src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+		
+		try {   
+            contar(parametroUm, parametroDois);		
+		} catch (ParametrosInvalidosException exception) {
+			System.out.println(exception.getMessage());
+		}
+    }	
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if ( parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        //Imprimir os números com base na variável contagem
+		int contagem = parametroDois - parametroUm;
+        for (int i=1; i <= contagem; i++ ) {
+            System.out.println("Imprimindo o numero " + i);            
+        }
+	}
+}
